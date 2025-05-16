1cdc20014cd526afcc1c69e18843375118205a5f
GalLu707 <zasseuwu@gmail.com>
2025-05-16 14:40:23 -0600

    ola

---
+++ cifrado_lab/src/cifrado_lab/Cifrado_lab.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cifrado_lab;
+
+/**
+ *
+ * @author USER
+ */
+public class Cifrado_lab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola como estas");
+    }
+    
+}
